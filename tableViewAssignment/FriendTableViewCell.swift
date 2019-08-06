ac129c35bbb2e653fa5cc1d71229ee8c9e9c326d
Phone Zaw Myint <lovekoko901@gmail.com>
2019-08-07 03:46:44 +0630

    no message

---
+++ tableViewAssignment/FriendTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  FriendTableViewCell.swift
+//  tableViewAssignment
+//
+//  Created by Aung Ko Ko on 8/7/19.
+//  Copyright © 2019 Phone Zaw Myint. All rights reserved.
+//
+
+import UIKit
+
+class FriendTableViewCell: UITableViewCell {
+static let identifier = "FriendTableViewCell"
+    
+    @IBOutlet weak var ivFaceImage: UIImageView!
+    @IBOutlet weak var viewFollow: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        self.selectionStyle = .none
+        viewFollow.layer.cornerRadius = 15
+        viewFollow.layer.borderWidth = 2
+        viewFollow.layer.borderColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        
+        ivFaceImage.layer.cornerRadius = ivFaceImage.frame.width / 2
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ tableViewAssignment/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  tableViewAssignment
+//
+//  Created by Aung Ko Ko on 8/6/19.
+//  Copyright © 2019 Phone Zaw Myint. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    static let identifier = "ViewController"
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        let nib = UINib(nibName: FriendTableViewCell.identifier, bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: FriendTableViewCell.identifier)
+        self.tableView.contentInset = UIEdgeInsets(top: 2, left: 0, bottom: 14, right: 0)
+        self.tableView.rowHeight = 130
+    }
+}
+
+extension ViewController: UITableViewDataSource , UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: FriendTableViewCell.identifier, for: indexPath) as! FriendTableViewCell
+        return cell
+    }
+    
+    
+    
+}
